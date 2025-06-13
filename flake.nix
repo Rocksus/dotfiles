5600ee13da3b0f3457a754dda1dfa9071a40bcb2
Ray Antonius <rayantonius@gmail.com>
2025-06-13 06:56:43 +0000

    add home-manager to outputs

--- flake.nix
+++ flake.nix
@@ -11,13 +11,11 @@
     };
  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
         ./configuration.nix
 
 	home-manager.nixosModules.home-manager
