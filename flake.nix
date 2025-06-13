bd9f13d15bd4a43ecac65188e3a5bc704b193dbb
Ray Antonius <rayantonius@gmail.com>
2025-06-13 06:55:26 +0000

    fix typo

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
  };
 
   outputs = { self, nixpkgs, ... }@inputs: {
