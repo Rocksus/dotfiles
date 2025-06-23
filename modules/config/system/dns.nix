025cd40c4088242f8cb3bb3caa020b0fdaf7558e
Ray Antonius <rayantonius@gmail.com>
2025-06-23 17:46:34 +0700

    fix: typo

--- modules/config/system/dns.nix
+++ modules/config/system/dns.nix
@@ -1,10 +1,12 @@
+{ config, ... }:
 {
-  networking.resolvconf.enable = true;
-  networking.resolvconf.dnsServers = [
+  networking.nameservers = [
     "208.67.222.222"  # OpenDNS primary
     "208.67.220.220"  # OpenDNS secondary
-    "8.8.8.8"         # Google DNS backup
-    "8.8.4.4"         # Google DNS backup
+    "8.8.8.8"         # Google fallback
+    "8.8.4.4"         # Google fallback
   ];
-  networking.resolvconf.dnsSearch = [ "" ];
+
+  # If you need to override or clear the 'search' line, set:
+  networking.search = [ ];
 }
