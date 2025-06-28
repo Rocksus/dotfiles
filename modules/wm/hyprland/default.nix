54be31694d0e8ae8bf2245527f58e62435380834
Ray Antonius <rayantonius@gmail.com>
2025-06-28 15:24:13 +0700

    feat: remove xdg portal for now

--- modules/wm/hyprland/default.nix
+++ modules/wm/hyprland/default.nix
@@ -4,19 +4,6 @@
     xwayland.enable = true;
   };
 
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    config = {
-      common.default = ["gtk"];
-      hyprland.default = ["gtk" "hyprland"];
-    };
-
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
-
   # Enable ozone wayland support
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
