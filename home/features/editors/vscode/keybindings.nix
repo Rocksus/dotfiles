1d1a7d47e5bc33794fca9f1ab7d0f8805250a679
Rocksus <rayantonius@gmail.com>
2025-07-03 19:42:58 +0700

    feat(vscode): update keybindings

--- home/features/editors/vscode/keybindings.nix
+++ home/features/editors/vscode/keybindings.nix
@@ -1,19 +1,27 @@
 {
   programs.vscode.profiles.default.keybindings = [
     {
-      "key" = "ctrl+`";
-      "command" = "workbench.action.terminal.focus";
-      "when" = "editorTextFocus";
+      key = "ctrl+`";
+      command = "workbench.action.terminal.focus";
+      when = "editorTextFocus";
     }
     {
-      "key" = "ctrl+`";
-      "command" = "workbench.action.focusActiveEditorGroup";
-      "when" = "terminalFocus";
+      key = "ctrl+`";
+      command = "workbench.action.focusActiveEditorGroup";
+      when = "terminalFocus";
     }
     {
-      "key" = "alt+f";
-      "command" = "editor.action.formatDocument";
-      "when" = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
+      key = "alt+shift+f";
+      command = "editor.action.formatDocument";
+      when = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
+    }
+    {
+      key = "alt+left";
+      command = "workbench.action.navigateBack";
+    }
+    {
+      key = "alt+right";
+      command = "workbench.action.navigateForward";
     }
   ];
 }
