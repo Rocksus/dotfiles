a8af749d4cc15eaf255d5f4f79817cd51ba1d0ca
Ray Antonius <rayantonius@gmail.com>
2025-07-16 11:11:39 +0700

    feat: enable screen sharing for wayland

--- home/features/desktop/hyprland/hyprland.nix
+++ home/features/desktop/hyprland/hyprland.nix
@@ -184,5 +184,22 @@ in {
         ];
       };
     };
+
+    # enable xdg for screen sharing
+    # https://discourse.nixos.org/t/hyprland-screen-sharing/43658/5
+    # https://wiki.hypr.land/Useful-Utilities/Screen-Sharing/
+    # https://github.com/fufexan/dotfiles/blob/main/system/programs/xdg.nix
+    xdg = {
+      portal = {
+        enable = true;
+        xdgOpenUsePortal = true;
+        common.default = ["gtk"];
+        hyprland.default = ["gtk" "hyprland"];
+      };
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-hyprland
+      ];
+    };
   };
 }
