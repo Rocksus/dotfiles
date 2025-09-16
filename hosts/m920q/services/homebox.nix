d6b662ac058feecd84c79a6038e1d373fe2ba2a1
Ray Antonius <rayantonius@gmail.com>
2025-09-16 09:44:04 +0700

    feat: add open firewall for homebox port

--- hosts/m920q/services/homebox.nix
+++ hosts/m920q/services/homebox.nix
@@ -1,10 +1,23 @@
-{
+{ 
+  lib, 
+  config, 
+  ... 
+}:
+let
+  port = 7745;
+in {
   services.homebox = {
     enable = true;
+    settings = {
+      HBOX_WEB_PORT = port;
+    };
   };
   systemd.services.homebox = {
     environment = {
       # see https://homebox.software/en/configure/ for options
     };
+  };
+  networking.firewall = lib.mkIf config.openFirewall {
+    allowedTCPPorts = [ port ];
   };
 }
