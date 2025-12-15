92fbd122d815e50de51cd1637e35589da924e934
Rocksus <rayantonius@gmail.com>
2025-12-15 17:32:37 +0700

    feat: add overlay

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -22,6 +22,7 @@
       outputs.overlays.stable-packages
 
       inputs.rust-overlay.overlays.default
+      inputs.authentik-nix.nixosModules.default
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
