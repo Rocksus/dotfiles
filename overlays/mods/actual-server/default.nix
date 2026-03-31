9440add523528e5efb5d27f840e300d5d0a708c2
Ray Antonius <rayantonius@gmail.com>
2026-03-31 08:59:16 +0700

    fix: update hash for actual server

--- overlays/mods/actual-server/default.nix
+++ overlays/mods/actual-server/default.nix
@@ -13,6 +13,6 @@ prev.actual-server.overrideAttrs (oldAttrs: rec {
   # https://nixos.org/manual/nixpkgs/stable/#javascript-yarn-v3-v4
   offlineCache = prev.yarn-berry.fetchYarnBerryDeps {
     inherit (oldAttrs) src missingHashes;
-    hash = "";
+    hash = "sha256-wdOIUYtiLbAkLngl+hIB/TlMLuX/YWZ9dt+a4qm+Fp8=";
   };
 })
